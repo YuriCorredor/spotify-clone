e0ad6d545a35e110f302e7fb8fbe27dfbb50c03b
Yuri Corredor <YuriCorredor159@gmail.com>
2022-02-15 10:21:37 -0300

    Login page done

--- pages/login.js
+++ pages/login.js
@@ -4,7 +4,19 @@ import Spotify from "../public/spotify-logo.png"
 
 export default function Login({ providers }) {
 
-    return <></>
+    return (
+        <div className="flex h-screen flex-col justify-center items-center bg-black">
+            <Image alt="spotify-logo" width={250} height={250} src={Spotify} />
+            {Object.values(providers).map( provider =>
+                <div key={provider.name}>
+                    <button
+                        onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+                        className="bg-[#1ed760] p-5 m-2 rounded-full font-bold" 
+                    >Logue com {provider.name}</button>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export async function getServerSideProps() {
